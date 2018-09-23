e91dd3dd5fc3d7747aae14c830cc6375636049b9
Alexander Walz <awalz@awalz-l.corp.instructure.com>
2018-09-22 20:38:21 -0600

    attempted fix for Cannot Get /

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,6 @@ import HomePage from './homepage/HomePage.js'
 import NotFound from './global/NotFound'
 import Login from './login/View'
 import Signup from './signup/View'
-import { TransitionGroup, CSSTransition } from "react-transition-group";
 import { Route, withRouter, Switch } from "react-router-dom";
 import Profile from './profile/View'
 import ConfirmPage from './Confirm'
@@ -16,18 +15,9 @@ import ExpiredLink from './global/forgotPassword/ExpiredLink'
 
 class App extends Component {
   render() {
-    const currentKey = this.props.location.pathname.split("/")[1] || "/";
-    const timeout = { enter: 300, exit: 200 };
     return (
-      <TransitionGroup component="main" className="page-main" style={{ height: '100%' }}>
-        <CSSTransition
-          key={currentKey}
-          timeout={timeout}
-          classNames="fade"
-          appear
-        >
           <div>
-            <Switch location={this.props.location}>
+            <Switch>
               <Route path="/" exact component={HomePage} />
               <Route path="/login" exact component={Login} />
               <Route path="/signup" exact component={Signup} />
@@ -39,10 +29,7 @@ class App extends Component {
               <Route path='/404' component={NotFound} />
               <Route component={NotFound} />
             </Switch>
-
           </div>
-        </CSSTransition>
-      </TransitionGroup>
     );
   }
 }
